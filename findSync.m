3c48af99f9514c4566de227f45e1a5e88e10ead7
Olof Sjödin <me@olofsjodin.se>
2017-09-04 10:51:04 +0200

    display an error message if there is no sync in the stream

--- findSync.m
+++ findSync.m
@@ -5,7 +5,7 @@ function [syncIndex, packetLength, packet] = findSync(syncHex, dataStream)
     syncIndex = 0;
     k = 1;
     
-    syncSeq = hexStrToBinArr(syncHex)
+    syncSeq = hexStrToBinArr(syncHex);
     % This sync search should do it with a certain confidence
     % so that if 28 bits of 32 match it will consider it as a match
 
@@ -28,36 +28,41 @@ function [syncIndex, packetLength, packet] = findSync(syncHex, dataStream)
             break;
         end
     end
-    
-    %% Retrieve the length parameter
+    if foundSync == 1
+        %% Retrieve the length parameter
 
-    possiblePacket = dataStream(syncIndex:end);
+        possiblePacket = dataStream(syncIndex:end);
 
-    % Retrieve the Length parameter from the data stream
-    % the length parameter is 1 byte i.e. 8 bits
+        % Retrieve the Length parameter from the data stream
+        % the length parameter is 1 byte i.e. 8 bits
 
-    % This is the position of the first bit
-    lengthParamLocation = length(syncSeq) + 1;
-    lengthBitVector = possiblePacket(lengthParamLocation:lengthParamLocation + 8);
+        % This is the position of the first bit
+        lengthParamLocation = length(syncSeq) + 1;
+        lengthBitVector = possiblePacket(lengthParamLocation:lengthParamLocation + 8);
 
-    % Calculate the length in decimal
-    packetLength = 0;
-    base = 2;
-    baseSum = 1;
+        % Calculate the length in decimal
+        packetLength = 0;
+        base = 2;
+        baseSum = 1;
 
-    flippedLenBitVector = fliplr(lengthBitVector);
+        flippedLenBitVector = fliplr(lengthBitVector);
 
-    for i = 1:length(flippedLenBitVector)
-        packetLength = packetLength + baseSum*flippedLenBitVector(i);
-        baseSum = baseSum*base;
+        for i = 1:length(flippedLenBitVector)
+            packetLength = packetLength + baseSum*flippedLenBitVector(i);
+            baseSum = baseSum*base;
+        end
+
+        % The length parameter is in bytes
+
+        %% Return packet
+
+        % TODO: require that the the packet length is smaller than the array
+        % TODO: handle if there aren't a sync in the packet stream
+        lengthParamLocation
+        packetLength
+        size(possiblePacket)
+        packet = possiblePacket(lengthParamLocation + 8:8*packetLength);
+    else
+        disp('No sync found');
     end
-
-    % The length parameter is in bytes
-    
-    %% Return packet
-    
-    % TODO: require that the the packet length is smaller than the array
-    % TODO: handle if there aren't a sync in the packet stream
-    
-    packet = possiblePacket(lengthParamLocation + 8:8*packetLength);
 end
