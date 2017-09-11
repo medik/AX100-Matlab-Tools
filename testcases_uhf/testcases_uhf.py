c15d86bc28393aa05b5166744e6343e2ece15cbb
Olof Sjödin <me@olofsjodin.se>
2017-09-11 17:02:58 +0200

    add function to set the parameter table, add function to reboot, set raw mode and RS, add testcases

--- testcases_uhf/testcases_uhf.py
+++ testcases_uhf/testcases_uhf.py
@@ -28,10 +28,41 @@ def custom_multi(n, msg, delay):
     for i in range(n):
         print(gen_custom_msg(msg_int_arr, delay))
 
+def set(table, mem, param, delay):
+    print("0 0 0 rparam download 20 " + str(table))
+    print(str(delay) + " 0 0 rparam set " + str(mem) + " " + str(param))
+
+def reboot(delay):
+    print(str(delay) + " 0 0 reboot 20")
+
+def enter_raw_mode():
+    set(0, "mode", 1, 100)
+
+def enter_normal_mode():
+    set(0, "mode", 2, 100)
+
+def enable_rs():
+    set(0, "fcs", 1, 100)
+
 def main():
     #print("Generating testcases")
-    ping_multi(10, 1, 1000, 100)
+    enable_rs()
+
+    enter_raw_mode()
+    for i in range(5):
+        ping_multi(10, i, 1000, 100)
+        ping_multi(10, i, 1000, 10)
+
     custom_multi(10, "FFFFFFFFFF", 1000)
+
+    enter_normal_mode()
+
+    for i in range(1,5):
+        ping_multi(10, i, 1000, 100)
+        ping_multi(10, i, 1000, 10)
+
+    custom_multi(10, "FFFFFFFFFF", 1000)
+
 
 if __name__ == "__main__":
     main()
