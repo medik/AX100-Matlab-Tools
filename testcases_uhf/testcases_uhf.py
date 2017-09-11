2850cab6a3afea2b0ad612a1fe4f7dcf922dd733
Olof Sjödin <me@olofsjodin.se>
2017-09-11 16:38:36 +0200

    remove arguments, add destination 20, add parameters to gen_ping, add ping_multi function

--- testcases_uhf/testcases_uhf.py
+++ testcases_uhf/testcases_uhf.py
@@ -2,26 +2,24 @@ import sys
 
 def gen_custom_msg(int_arr, delay):
     # int_arr is an array of integers from 0 to 256
-    
+
     toSend = ""
 
     for i in int_arr:
         toSend += str(i) + " "
 
-    return str(delay) + " 0 0 obckth tc " + toSend
+    return str(delay) + " 0 0 obckth tc 20 " + toSend
 
-def gen_ping(delay, size):
-    return str(delay) + " 0 0 ping 1 1 " + str(size)
+def gen_ping(destination, delay, timeout, size):
+    return str(delay) + " 0 0 ping " + str(destination) + " " + str(timeout) + " " + str(size)
+
+def ping_multi(n, dest, interval, size):
+    for i in range(n):
+        print(gen_ping(dest, interval, size))
 
 def main():
     #print("Generating testcases")
-    
-    n = int(sys.argv[1])
-    interval = int(sys.argv[2])
-    size = int(sys.argv[3])
-    
-    for i in range(n):
-        print(gen_ping(interval, size))
+    ping_multi(10, 1, 1000, 100)
 
 if __name__ == "__main__":
     main()
