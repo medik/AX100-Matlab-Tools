d4b92d5d6539568cf4dfc0c1ef5de77c6522abe9
Olof Sjödin <me@olofsjodin.se>
2017-09-20 19:40:38 +0200

    update

--- Decapsulator/analyse_file.m
+++ Decapsulator/analyse_file.m
@@ -2,9 +2,10 @@ function analyse_file(filename, syncSeq, outputname)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
     fileID = fopen(filename);
-    dataStreamInt = fread(fileID, 'ubit8');
+    dataStreamInt = fread(fileID, 'uint8');
     
-    packetHandler(strcat(outputname,'-hex.csv'), dataStreamInt, syncSeq, 0);
+    resp = packetHandler(strcat(outputname,'-hex.csv'), dataStreamInt, syncSeq, 0);
+    disp(strcat('Response:', binArrToHexStr(resp)))
     %packetHandler(strcat(outputname,'-binary.csv'), dataStreamInt, syncSeq, 1);
     fclose(fileID);
 end
