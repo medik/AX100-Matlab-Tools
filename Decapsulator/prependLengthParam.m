3f7076bdd071cfc140d10de305fcd5483da8426e
Olof Sjödin <me@olofsjodin.se>
2017-09-21 12:23:44 +0200

    include length byte

--- Decapsulator/prependLengthParam.m
+++ Decapsulator/prependLengthParam.m
@@ -1,7 +1,7 @@
 function ret = prependLengthParam( packet )
 %PREPENDLENGTHPARAM Summary of this function goes here
 %   Detailed explanation goes here
-    len = length(packet)/8; % length in bytes
+    len = length(packet)/8 + 1; % length in bytes
     ret = [decToBinArr(len) packet];
 
 end
